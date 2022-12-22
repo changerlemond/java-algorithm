fb1a7d47a21fc361d62c40474ba3bb45bccabd4c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2022-12-22 19:41:30 +0900

    initial commit

---
+++ src/main/java/Max3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Max3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("세 정수의 최댓값을 구합니다.");
+
+        System.out.println("a의 값: ");
+        int a = scanner.nextInt();
+
+        System.out.println("b의 값: ");
+        int b = scanner.nextInt();
+
+        System.out.println("c의 값: ");
+        int c = scanner.nextInt();
+
+        System.out.println("최댓값은 " + Max3Method.max3(a, b, c) + "입니다.");
+    }
+}
+
+class Max3Method {
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+
+        return max;
+    }
+}
---
+++ src/main/java/Min3.java
@@ -0,0 +1,12 @@
+public class Min3 {
+
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if (min > b)
+            min = b;
+        if (min > c)
+            min = c;
+
+        return min;
+    }
+}
