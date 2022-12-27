99053b16621f75524c14ac8137182f0795e52a2e
changerlemond <83348294+changerlemond@users.noreply.github.com>
2022-12-28 01:01:59 +0900

    for - while + sumGauss

---
+++ src/main/java/SumGauss.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumGauss {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1에서 n 까지의 총합을 구합니다.");
+        System.out.println("n의 값: ");
+        int n = scanner.nextInt();
+
+        int sum = (n + 1) * (n / 2) + (n % 2 == 1 ? (n + 1) / 2 : 0);
+
+        System.out.println("1에서 "+ n + "까지의 총합은 "+ sum + "입니다.");
+    }
+}
---
+++ src/main/java/SumWhile.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SumWhile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.println("n값: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+//        int i = 1;
+
+//        1. while문 사용 방법
+//        while (i <= n) {
+//            sum += i;
+//            i++;
+//        }
+
+//        2. for문 사용 방법
+
+        for (int i = 1; i <= n; i++)
+            sum += i;
+
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+    }
+
+}
