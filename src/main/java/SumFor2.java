311c78cb81d25cbb31b40f2236fc26048927537b
changerlemond <83348294+changerlemond@users.noreply.github.com>
2022-12-29 17:07:38 +0900

    while문으로 로직 만들기

---
+++ src/main/java/SumFor2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumFor2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int b;
+
+        System.out.println("a의 값: ");
+        int a = scanner.nextInt();
+
+        while (true) {
+            System.out.println("b의 값: ");
+            b = scanner.nextInt();
+
+            if (b > a) break;
+            System.out.println("a보다 큰 값을 입력하세요!");
+        }
+
+        System.out.println("b - a는 " + (b - a) + "입니다.");
+
+    }
+
+}
